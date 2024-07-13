211b1657b9daece4cd0a6cd69b580d175a19e1c2
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-07-13 19:14:54 +0300

    nuxt 1 - porto 2024

---
+++ pages/projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+    <h2>My Projects </h2>
+    <div class="my-projects-box"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
