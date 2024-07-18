d3967b38a80132e09d521547230649868408cf19
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-07-19 02:00:36 +0300

    v33

--- components/AllProjectsCompo.vue
+++ components/AllProjectsCompo.vue
@@ -1,52 +1,47 @@
 <template>
   <div>
     <div class=" "  >
-        <!-- ............................................................................................ -->
-      <!-- <v-row> -->
-        <!-- <v-col  cols="12" md="4" > -->
-          <v-card  rounded class="flex orange darken-2 mt-2 pt-1 " >
-            <a :href='proj.image' target="_blank">
-            <v-img
-            class="align-end text-white"
-            height="200"
-            :src="require(`../assets/img/${proj.src}`)"
-            cover>
-            <v-card-title class="grey darken-3 orange--text pa-2">{{proj.title}}</v-card-title>
-            </v-img></a>
-            <v-card-subtitle class="pt-4">
-            Number {{proj.id}}
-            </v-card-subtitle>
-            <v-card-text>
-            <div>{{proj.techno}}</div>
-            </v-card-text>
-            <v-card-actions class="pb-9">
-
-                <a class="mx-2" :href='proj.github' target="_blank">
-                  <v-btn class="grey darken-3 orange--text" text="Github">Github</v-btn>
-                </a>
-                <v-dialog
-                  v-model="dialog4"
-                  width="900"
-                  >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn v-bind="attrs" v-on="on" class="orange lighten-4" color="orange" text="Explore">Explore</v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title class="text-h5 orange lighten-2">
-                      Watch {{proj.id}} Project Video
-                    </v-card-title>
-                    <v-card-actions>
-                    <iframe rounded class="orange pa-2 my-2 mx-auto center "
-                    :src='proj.explore'
-                    height="465" width="504" frameborder="0" allowfullscreen="" title="منشور مضمن"></iframe>
-                    </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-          </v-card>
-          <!-- /<v-col> -->
-      <!-- </v-row> -->
-    <!-- ............................................................................................ -->
+      <!-- ............................................................................................ -->
+      <v-card  rounded class="flex orange darken-2 mt-2 pt-1 " >
+        <a :href='proj.image' target="_blank">
+        <v-img
+        class="align-end text-white"
+        height="200"
+        :src="require(`../assets/img/${proj.src}`)"
+        cover>
+        <v-card-title class="grey darken-3 orange--text pa-2">{{proj.title}}</v-card-title>
+        </v-img></a>
+        <v-card-subtitle class="pt-4">
+        Number {{proj.id}}
+        </v-card-subtitle>
+        <v-card-text>
+        <div>{{proj.techno}}</div>
+        </v-card-text>
+        <v-card-actions class="pb-9">
+            <a class="mx-2" :href='proj.github' target="_blank">
+              <v-btn class="grey darken-3 orange--text" text="Github">Github</v-btn>
+            </a>
+            <v-dialog
+              v-model="dialog4"
+              width="900"
+              >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" class="orange lighten-4" color="orange" text="Explore">Explore</v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="text-h5 orange lighten-2">
+                  Watch {{proj.id}} Project Video
+                </v-card-title>
+                <v-card-actions>
+                <iframe rounded class="orange pa-2 my-2 mx-auto center "
+                :src='proj.explore'
+                height="465" width="504" frameborder="0" allowfullscreen="" title="منشور مضمن"></iframe>
+                </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card-actions>
+      </v-card>
+  <!-- ............................................................................................ -->
     </div>
 
   </div>
@@ -58,7 +53,6 @@ export default {
   props:["proj"],
   data() {
     return {
-    //  props:["projec"]
   }
 }
 
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,140 +1,291 @@
 <template>
- <div class="orange my-6 py-6 px-4 rounded">
-  <v-card class="pa-2">
-   <v-card-title class="headline">My Projects
-   </v-card-title>
-   <v-card-text>
+  <div class="orange my-6 py-6 px-4 rounded">
+    <v-card class="pa-2">
+      <v-card-title class="headline">My Projects</v-card-title>
+    <v-card-text>
 
-     <v-text-field
+    <v-text-field
       v-model="search"
       required
       color="orange"
-      class="grey--text grey darken-3 rounded pa-3 ma-5"
+      class="orange--text grey darken-3 rounded px-5 ma-2"
       append-icon="mdi-magnify"
       prepend-icon="mdi-heart"
-     ></v-text-field>
+    ></v-text-field>
 
-<!-- /// -->
-
-  <!-- <v-row> -->
-     <v-container fluid>
+    <v-container >
       <v-row dense>
         <v-col cols="12" md="4"
-          v-for="proj in searchProject" :key="proj.id"
+        v-for="proj in searchProject" :key="proj.id"
         >
-        <div class=" "  >
-        <AllProjectsCompo :proj="proj"  > </AllProjectsCompo>
-      </div>
-
-      </v-col>
+          <div class=" "  >
+            <AllProjectsCompo :proj="proj"  > </AllProjectsCompo>
+          </div>
+        </v-col>
       </v-row>
-      </v-container>
-
-
-<!-- /// -->
-        <br>
-        <hr class="my-3">
-        <br>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn
-          color="grey lighten-2"
-          class="orange--text"
-          nuxt
-          to="/skills"
-        >
-          Continue
-        </v-btn>
-      </v-card-actions>
-      </v-card>
+    </v-container>
+    <br>
+    <hr class="my-3">
+    <br>
+    </v-card-text>
+    <v-card-actions>
+    <v-spacer />
+    <v-btn
+      color="grey lighten-2"
+      class="orange--text"
+      nuxt
+      to="/skills"
+    >
+    Continue
+    </v-btn>
+    </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
-import AllProjectsCompo from '~/components/AllProjectsCompo.vue'
-export default {
-  name: "projects",
-  components:{AllProjectsCompo},
-  data () {
-    return {
-      dialog: false,
-      dialog2:false,
-      dialog3:false,
-      dialog4: false,
-      search: '',
-      projects: [
-        {
-        id: 1,
-        title:"name 1 ",
-        techno: "html , css",
-        github: "",
-        image: 'https://www.linkedin.com/posts/sara-darwish-d_wordpress-wordpress-project-activity-7168527384273534976-nmbF?utm_source=share&utm_medium=member_desktop',
-        explore: '',
-        src:'wordpress_somoq_jazeel.png',
-      },
-      {
-        id: 2,
-        title:"name 2 ",
-        techno: "HTML - CSS - JS - React js ",
-        github: "https://github.com/SaraDrwish/reaaaacttttt23",
-        image: 'https://github.com/SaraDrwish/reaaaacttttt23',
-        explore: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7168527361460711424',
-        src:'react20.png',
-        },
-      {
-        id: 3,
-        title:"name 3 ",
-        techno: " vue js , nuxt js , vuetify ",
-        github: "https://github.com/SaraDrwish/template1",
-        image: '',
-        explore: '',
-        src:'wordpress_somoq_jazeel.png',
-        },
-      {
-        id: 4,
-        title:"name 4 ",
-        techno: " CSS - HTML - JS ",
-        github: "https://github.com/SaraDrwish/todo2023",
-        image: 'https://saradrwish.github.io/todo2023/',
-        explore: 'https://saradrwish.github.io/todo2023/',
-        src:'todo.png',
-        },
-      {
-        id: 5,
-        title:"name 5 ",
-        techno: " CSS - HTML - JS - vue js ",
-        github: "https://github.com/SaraDrwish/SolutionPlusTasks1/tree/main/task4crud",
-        image: 'https://github.com/SaraDrwish/SolutionPlusTasks1/tree/main/task4crud',
-        explore: ' ',
-        src:'vueNote.png',
-        },
-      {
-        id: 6,
-        title:"name6 ",
-        techno: " JS - vue js ",
-        github: "https://github.com/SaraDrwish/SolutionPlusTasks1/tree/main/crud-tsk4-todo",
-        image: 'https://github.com/SaraDrwish/SolutionPlusTasks1/tree/main/crud-tsk4-todo',
-        explore: ' ',
-        src:'todoVue.png',
-        },
-      {
-        id: 7,
-        title:"name 7 ",
-        techno: "HTML - CSS - JS - Vue js - vuetify - Nuxt js 2 ",
-        github: "https://gitlab.com/it.saradarwish/gitflow-task5-vuetify-v1",
-        image: 'https://gitlab.com/it.saradarwish/gitflow-task5-vuetify-v1',
-        explore: ' ',
-        src:'dashVuenuxt2.png',
-        },
+  import AllProjectsCompo from '~/components/AllProjectsCompo.vue'
+  export default {
+    name: "projects",
+    components:{AllProjectsCompo},
+    data () {
+      return {
+        dialog: false,
+        dialog2:false,
+        dialog3:false,
+        dialog4: false,
+        search: '',
+        projects: [
+          {
+            id: 1,
+            title:"Template Watch",
+            techno: "CSS - HTML",
+            github: "https://github.com/SaraDrwish/task19",
+            image: 'https://saradrwish.github.io/task19/',
+            explore: '',
+            src:'1watch.png',
+          },
+          {
+            id: 2,
+            title:"Template Plant",
+            techno: "HTML - CSS - Bootstrap - JS ",
+            github: "https://github.com/SaraDrwish/temp1_bootstrab_plant",
+            image: 'https://saradrwish.github.io/temp1_bootstrab_plant/',
+            explore: ' ',
+            src:'2plant.png',
+          },
+          {
+            id: 3,
+            title:"Template 3 Jeans",
+            techno: "HTML - CSS",
+            github: "https://github.com/SaraDrwish/template1",
+            image: 'https://saradrwish.github.io/template1/',
+            explore: 'https://saradrwish.github.io/template1/',
+            src:'jeans3.png',
+          },
+          {
+            id: 4,
+            title:"Template Fish Shift",
+            techno: "HTML - CSS",
+            github: "https://github.com/SaraDrwish/template2",
+            image: 'https://saradrwish.github.io/template2/',
+            explore: 'https://saradrwish.github.io/todo2023/',
+            src:'shift4.png',
+          },
+          {
+            id: 5,
+            title:"Template Bakrey",
+            techno: "HTML - CSS",
+            github: "https://github.com/SaraDrwish/task13full",
+            image: 'https://saradrwish.github.io/task13full/',
+            explore: ' ',
+            src:'bary5.png',
+          },
+          {
+            id: 6,
+            title:"Template Filter",
+            techno: "HTML - CSS - JS",
+            github: "https://github.com/SaraDrwish/filter",
+            image: 'https://saradrwish.github.io/filter/',
+            explore: ' ',
+            src:'filt6.png',
+          },
+          {
+            id: 7,
+            title:"Template Prev&Next ",
+            techno: "CSS - HTML- JS",
+            github: "https://github.com/SaraDrwish/js_task_close_prev_next",
+            image: 'https://saradrwish.github.io/js_task_close_prev_next/',
+            explore: ' ',
+            src:'netprov7.png',
+          },
+          {
+            id: 8,
+            title:"Template CRUD ",
+            techno: "HTML - CSS - JS ",
+            github: "https://github.com/SaraDrwish/radwatasknewcrud24v1",
+            image: 'https://saradrwish.github.io/radwatasknewcrud24v1/',
+            explore: ' ',
+            src:'crd.png',
+          },
+          {
+            id:9 ,
+            title:"Tamplate Squers",
+            techno: "HTML - CSS - JS",
+            github: "https://github.com/SaraDrwish/task26",
+            image: 'https://saradrwish.github.io/task26/',
+            explore: ' ',
+            src:'squr26_9.png',
+          },
+          {
+            id:10 ,
+            title:"Template API",
+            techno: "HTML - CSS - JS",
+            github: "https://github.com/SaraDrwish/task28json",
+            image: 'https://saradrwish.github.io/task28json/',
+            explore: ' ',
+            src:'api10.png',
+          },
+          {
+            id:11,
+            title:"Template Gost",
+            techno: "HTML - CSS",
+            github: "https://github.com/SaraDrwish/template3ghost",
+            image: 'https://saradrwish.github.io/template3ghost/',
+            explore: '',
+            src:'gost11..png',
+          },
+          {
+            id:12,
+            title:"Template Leon",
+            techno: "HTML - CSS - JS",
+            github: "https://github.com/SaraDrwish/leonjs5",
+            image: 'https://saradrwish.github.io/leonjs5/',
+            explore: 'https://www.linkedin.com/posts/sara-darwish-d_zero-elzero-activity-6944739530486784000-vw7A?utm_source=share&utm_medium=member_desktop',
+            src:'leaon12-12.png',
+          },
+          {
+            id:13,
+            title:"Template Coffee",
+            techno: "HTML - CSS ",
+            github: "https://github.com/SaraDrwish/ProjectOne23",
+            image: 'https://saradrwish.github.io/ProjectOne23/',
+            explore: ' ',
+            src:'coff.png',
+          },
+          {
+            id:14,
+            title:"Template Space",
+            techno: "HTML - CSS ",
+            github: "https://github.com/SaraDrwish/Project2",
+            image: 'https://saradrwish.github.io/Project2/',
+            explore: ' ',
+            src:'14spac.png',
+          },
+          {
+            id:15,
+            title:"Template 15",
+            techno: "HTML - CSS - BootStrap",
+            github: "https://github.com/SaraDrwish/project3_bootstrap",
+            image: 'https://saradrwish.github.io/project3_bootstrap/',
+            explore: ' ',
+            src:'15bootsrr.png',
+          },
+          {
+            id:16,
+            title:"Template Todo",
+            techno: "HTML - CSS - JS",
+            github: "https://github.com/SaraDrwish/todo2023",
+            image: ' https://saradrwish.github.io/todo2023/',
+            explore: ' ',
+            src:'todo.png',
+          },
+          {
+            id:17,
+            title:"Template City Count",
+            techno: "HTML - CSS ",
+            github: "https://github.com/SaraDrwish/js_task1",
+            image: 'https://saradrwish.github.io/js_task1/',
+            explore: ' ',
+            src:'citycount.png',
+          },
+          {
+            id:18,
+            title:"Tamplate  Shopping",
+            techno: "HTML - CSS - JS ",
+            github: "https://github.com/SaraDrwish/app1-shopping",
+            image: 'https://saradrwish.github.io/app1-shopping/index.html',
+            explore: ' ',
+            src:'shopping.png',
+          },
+          {
+            id:19,
+            title:"Template Dashboard",
+            techno: "HTML - CSS ",
+            github: "https://github.com/SaraDrwish/pro1cha2",
+            image: 'https://saradrwish.github.io/pro1cha2/',
+            explore: ' ',
+            src:'sashbo.png',
+          },
+          {
+            id:20,
+            title:"Template Portfolio",
+            techno: "HTML - CSS - JS - React js",
+            github: "https://github.com/SaraDrwish/reaaaacttttt23",
+            image: 'https://saradrwish.github.io/reaaaacttttt23/',
+            explore: ' ',
+            src:'react20.png',
+          },
+          {
+            id:21,
+            title:"Template Beauty",
+            techno: "HTML - CSS - JS - React js",
+            github: "https://github.com/SaraDrwish/srpo-react",
+            image: 'https://github.com/SaraDrwish/srpo-react',
+            explore: ' ',
+            src:'react2.jpeg',
+          },
+          {
+            id:22,
+            title:"Template  22",
+            techno: "HTML - CSS - JS - React js",
+            github: "https://github.com/SaraDrwish/react3Porto",
+            image: 'https://saradrwish.github.io/react3Porto/',
+            explore: ' ',
+            src:'react22.png',
+          },
+          {
+            id:23 ,
+            title:"Template  23",
+            techno: "HTML - CSS - JS  ",
+            github: "https://github.com/SaraDrwish/myportofolio2022march",
+            image: 'https://saradrwish.github.io/myportofolio2022march/',
+            explore: ' ',
+            src:'my23port.png',
+          },
+          {
+            id:24 ,
+            title:"Template  24",
+            techno: "HTML - CSS - JS  ",
+            github: "https://github.com/SaraDrwish/myportofolio2022march",
+            image: 'https://saradrwish.github.io/myportofolio2022march/',
+            explore: ' ',
+            src:'my23port.png',
+          },
 
-    ]
-    }
-  },
-  computed: {
-    searchProject() {
+
+
+
+
+
+
+        ]
+      }
+    },
+    computed: {
+      searchProject() {
       return this.projects.filter(proj => proj.techno.toLocaleLowerCase().includes(this.search.toLocaleLowerCase()))
     }
+    }
   }
-}
 </script>
